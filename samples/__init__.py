64e1cdb47bdce67d1fd0046b0641c5106246e167
FernandoCelmer <email@fernandocelmer.com>
2023-08-23 01:14:31 -0300

    ⚙️ FEATURE: Example Update

---
+++ samples/__init__.py
@@ -0,0 +1,10 @@
+from samples.model_dataclass import UserDataClass
+from samples.model_pydantic import UserPydantic
+from samples.model_sqlalchemy import UserSQLAlchemy
+
+
+__all__ = [
+    "UserDataClass",
+    "UserPydantic",
+    "UserSQLAlchemy"
+]
