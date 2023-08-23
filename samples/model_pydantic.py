178bf5bf0713e0cbd9694a53ead9e9d6bdf06ea3
FernandoCelmer <email@fernandocelmer.com>
2023-08-23 01:12:00 -0300

    ⚙️ FEATURE: Pydantic Upgrade Model

--- samples/model_pydantic.py
+++ samples/model_pydantic.py
@@ -5,5 +5,5 @@ class UserPydantic(BaseModel):
     __tablename__ = 'user'
     __table_id__ = 'bigquery-orm.dataset.user'
 
-    id: int = Field(default=None)
-    email: str = Field(default=None)
+    id: int
+    email: str = Field(default=None, description="Customer Contact")
