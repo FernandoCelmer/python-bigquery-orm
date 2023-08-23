c3a0bafacff438d7bc0219a5da298599a9395781
FernandoCelmer <email@fernandocelmer.com>
2023-08-23 01:21:42 -0300

    ❤️️ TEST: Update

---
+++ tests/conftest.py
@@ -0,0 +1,48 @@
+import pytest
+
+
+@pytest.fixture
+def mock_base_field_input():
+    return {
+        "name": "id",
+        "field_type": "INTEGER",
+        "mode": "NULLABLE",
+        "description": "My ID",
+        "max_length": None
+    }
+
+
+@pytest.fixture
+def mock_base_field_output():
+    return {
+        "name": "id",
+        "type": "INTEGER",
+        "mode": "NULLABLE",
+        "description": "My ID",
+        "max_length": None
+    }
+
+
+@pytest.fixture
+def mock_base_table_keys():
+    return ['id', 'email']
+
+
+@pytest.fixture
+def mock_base_table_mapping():
+    return [
+        {
+            'name': 'id',
+            'type':'INTEGER',
+            'mode': 'REQUIRED',
+            'description': 'id',
+            'max_length': None
+        },
+        {
+            'name': 'email',
+            'type': 'STRING',
+            'mode': 'NULLABLE',
+            'description': 'Customer Contact',
+            'max_length': None
+        }
+    ]
---
+++ tests/test_table_base_field.py
@@ -0,0 +1,19 @@
+import pytest
+
+from tests.conftest import mock_base_field_input, mock_base_field_output
+from bigquery_orm.base.table import BaseField
+
+
+def test_base_field_instance_with_success(mock_base_field_input, mock_base_field_output):
+    base_field = BaseField(**mock_base_field_input)
+    assert base_field.properties == mock_base_field_output
+
+
+def test_base_field_instance_with_error(mock_base_field_input):
+    field = mock_base_field_input.copy()
+    field.pop("name")
+
+    with pytest.raises(TypeError) as error:
+        BaseField(**field)
+
+    assert "missing 1 required positional argument: 'name'" in error.value.args[0]
