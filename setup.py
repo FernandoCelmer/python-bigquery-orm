8e0ee76cfe4b4670fd206d29b53a84e56ed65302
FernandoCelmer <email@fernandocelmer.com>
2023-08-27 16:31:18 -0300

    ⚙️ FEATURE: Update

--- setup.py
+++ setup.py
@@ -31,11 +31,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],
-    packages=[
-        'bigquery_orm',
-    ],
     include_package_data=True,
     python_requires=">=3.6",
-    zip_safe=True,
-    fullname='bigquery-orm',
+    zip_safe=True
 )
