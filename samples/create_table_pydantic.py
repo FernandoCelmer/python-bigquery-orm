808aab6ea1571e8e87d9ee4abcecdeb1b64c7beb
FernandoCelmer <email@fernandocelmer.com>
2023-09-06 01:06:33 -0300

    ⚙️ FEATURE: Updating the samples

--- samples/create_table_pydantic.py
+++ samples/create_table_pydantic.py
@@ -1,11 +1,9 @@
 from google.cloud.bigquery import Client
 
 from bigquery_orm import Table
-
 from samples import UserPydantic
-from samples.client_bigquery import client
 
 
-def create_table_pydantic(client: Client = client) -> None:
+def create_table_pydantic(client: Client) -> None:
     table = Table(model_class=UserPydantic, client=client)
     table.create()
